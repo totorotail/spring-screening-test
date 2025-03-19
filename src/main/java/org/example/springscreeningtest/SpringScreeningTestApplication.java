c8f619d123d1724888a7f7adf90ee9235bf0d680
totorotail <jminkyun@gmail.com>
2025-03-19 22:13:16 +0900

    Initial commit

---
+++ src/main/java/org/example/springscreeningtest/SpringScreeningTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.springscreeningtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScreeningTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringScreeningTestApplication.class, args);
+  }
+
+}
