ab7e4c906ba852bf5e2601ba39485ca3d206365a
totorotail <jminkyun@gmail.com>
2025-03-20 00:10:10 +0900

    feat: HospitalRepository 생성

---
+++ src/main/java/org/example/springscreeningtest/hospital/repository/HospitalRepository.java
@@ -0,0 +1,11 @@
+package org.example.springscreeningtest.hospital.repository;
+
+import java.util.Optional;
+import org.example.springscreeningtest.hospital.entity.Hospital;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+
+}
