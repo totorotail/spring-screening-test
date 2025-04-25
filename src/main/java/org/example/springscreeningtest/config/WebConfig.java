109d9c6a44f8a76f146d429d48265be742c0623c
totorotail <jminkyun@gmail.com>
2025-04-25 17:45:55 +0900

    fix: WebConfig.java 수정

--- src/main/java/org/example/springscreeningtest/config/WebConfig.java
+++ src/main/java/org/example/springscreeningtest/config/WebConfig.java
@@ -16,16 +16,4 @@ public class WebConfig implements WebMvcConfigurer {
         .allowedHeaders("*")
         .allowCredentials(true);
   }
-
-  // 프론트엔드 라우터 경로를 서버가 index.html로 포워딩하게 처리
-  @Override
-  public void addViewControllers(ViewControllerRegistry registry) {
-    // ✅ 정적 자원은 제외하고, 프론트 라우터 경로만 index.html로 보내기
-    registry.addViewController("/{spring:[a-zA-Z0-9\\-]+}")
-        .setViewName("forward:/index.html");
-    registry.addViewController("/**/{spring:[a-zA-Z0-9\\-]+}")
-        .setViewName("forward:/index.html");
-    registry.addViewController("/{spring:[a-zA-Z0-9\\-]+}/**{spring:?!(\\.js|\\.css|\\.png|\\.svg|\\.ico|\\.json|\\.map)$}")
-        .setViewName("forward:/index.html");
-  }
 }
